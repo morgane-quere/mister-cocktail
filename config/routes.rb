a1d3671be93c397f1cf5e6f33267d8acd0e7c79e
morgane quere <qu.morgane@gmail.com>
2021-02-25 17:01:02 +0100

    controllers OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
